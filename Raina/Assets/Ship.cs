0b832b01cbfa6a31f7eab0acafcc217712cfd3a5
Pedro Montoto García <pedro.montoto@live.u-tad.com>
2017-01-16 19:09:45 +0100

    Recolected rain

--- Raina/Assets/Ship.cs
+++ Raina/Assets/Ship.cs
@@ -3,15 +3,26 @@ using System.Collections;
 
 public class Ship : MonoBehaviour {
     public float metersPerSecond = 1;
+    public float startingRain = 0;
+
+    [ReadOnly] public float rain = 0;
+
+    void Start() {
+        rain = startingRain;
+    }
 
 	void Update () {
         Vector3 position = transform.localPosition;
         position.x = position.x + Time.deltaTime * metersPerSecond;
-        if (position.x - 3 > 0.001f)
+        if (position.x - 1000 > 0.001f)
         {
             position.x = -position.x;
         }
 
         transform.localPosition = position;
 	}
+
+    void OnParticleCollision(GameObject particles) {
+        rain++;
+    }
 }
