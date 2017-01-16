9c73ff4f399bcad3fc1c8c2c85adc215af36fb49
Pedro Montoto García <pedro.montoto@live.u-tad.com>
2017-01-16 21:55:46 +0100

    Added collision

--- Raina/Assets/Ship.cs
+++ Raina/Assets/Ship.cs
@@ -2,27 +2,52 @@
 using System.Collections;
 
 public class Ship : MonoBehaviour {
+    /**
+     *  Forward acceleration
+     */
     public float metersPerSecond = 1;
+    public float anglePerSecond = 90;
     public float startingRain = 0;
+    public float maxVelocity = 1000;
 
     [ReadOnly] public float rain = 0;
 
+    private Rigidbody rigidBody;
+    private float startingMass;
+    private Vector3 oldVelocity;
+
     void Start() {
         rain = startingRain;
+        rigidBody = GetComponent<Rigidbody>();
+        startingMass = rigidBody.mass;
     }
 
-	void Update () {
-        Vector3 position = transform.localPosition;
-        position.x = position.x + Time.deltaTime * metersPerSecond;
-        if (position.x - 1000 > 0.001f)
-        {
-            position.x = -position.x;
+    void FixedUpdate() {
+        float forward = Input.GetAxis("Vertical");
+        forward = (forward > 0 ? forward : 0);
+
+        if (forward > 0) {
+            rigidBody.AddForce(metersPerSecond * transform.up, ForceMode.VelocityChange);
         }
 
-        transform.localPosition = position;
-	}
+        float spin = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.forward, spin * anglePerSecond * Time.deltaTime);
+
+        rigidBody.velocity = Vector3.ClampMagnitude(rigidBody.velocity, maxVelocity);
+        transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y);
+        Vector3 trueRotation = transform.localRotation.eulerAngles;
+        trueRotation.x = 0;
+        trueRotation.y = 0;
+        transform.localRotation = Quaternion.Euler(trueRotation);
+        oldVelocity = rigidBody.velocity;
+    }
 
     void OnParticleCollision(GameObject particles) {
         rain++;
+        rigidBody.mass = startingMass + rain;
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        transform.rotation = Quaternion.Euler(Vector3.Reflect(oldVelocity, collision.contacts[0].normal));
     }
 }
